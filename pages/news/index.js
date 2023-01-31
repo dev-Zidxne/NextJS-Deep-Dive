388a280a1ad6e7eb8ecd1c53849a0c37ab93173a
Zidane Innis <zidaneinnis@gmail.com>
2023-01-30 19:06:22 -0500

    Linking Between Pages

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,23 @@
 // our-domain.com/news
-import React from "react";
+
+import Link from "next/link";
+import { Fragment } from "react";
 
 function NewsPage() {
-  return <div>NewsPage</div>;
+  return (
+    <Fragment>
+      <div>NewsPage</div>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-f+ramework">
+            {" "}
+            NextJs is a Great Framework
+          </Link>
+        </li>
+        <li>Something else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
